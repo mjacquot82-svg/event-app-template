0d7dfe55d2ac7bc6484e130154064a503dd76865
mjacquot82-svg <mjacquot82@gmail.com>
2026-06-22 01:32:45 +0000

    match hero aspect ratio to cropped logo

--- frontend/src/components/HomecomingHero.tsx
+++ frontend/src/components/HomecomingHero.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Image, StyleSheet, View, useWindowDimensions } from 'react-native';
 
 const HERO_IMAGE = require('../../assets/images/whc-logo-cropped.png');
-const HERO_ASPECT_RATIO = 1880 / 400;
+const HERO_ASPECT_RATIO = 838 / 627;
 
 type HomecomingHeroProps = {
   title: string;
